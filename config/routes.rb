3cfa4aae1245c2164e0e614b6609bc9b466ef8f0
leoalcantara <leonidio.ti@gmail.com>
2018-08-27 12:39:50 -0300

    route add

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :contacts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => "contacts#index" #linha para adicionar rota para index do contato
+
 end
